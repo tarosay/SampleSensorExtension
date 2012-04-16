9a1a55dabb9dc8797036eb072bca7ffac7998dce
Tarosa <tarosa.y@gmail.com>
2012-04-16 19:12:58 +0900

    First commit

---
+++ gen/com/sonyericsson/extras/liveware/extension/sensorsample/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.sonyericsson.extras.liveware.extension.sensorsample;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
